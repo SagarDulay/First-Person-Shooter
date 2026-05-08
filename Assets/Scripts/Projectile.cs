a2371b85577c50103b8e2a1157089c90f9bddc3e
Sagar Dulay <sagardulay15@gmail.com>
2026-05-07 17:58:40 -0700

    added Projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float projectileSpeed;
+    private Rigidbody projectileRigidbody;
+
+
+    private void Awake()
+    {
+        projectileRigidbody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {     
+        Invoke("ResetBullet", 10f);
+        projectileRigidbody.linearVelocity = transform.forward = projectileSpeed;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void ResetBullet()
+    {
+        Destroy(gameObject);
+    }
+
+}
